1694c767ba7a335b657460436a3abb2bf667a677
Wanderson Teodoro Carvalho <wandersonteodorocarvalho@gmail.com>
2023-12-27 09:55:48 -0300

    Update condicionais2.py

--- Exercicio-Condicionais/condicionais2.py
+++ Exercicio-Condicionais/condicionais2.py
@@ -1,4 +1,4 @@
-# 64 Crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar quando o usuário digitar o
+# Crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar quando o usuário digitar o
 # valor 999, que e a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
 # desconsiderando o flag.
 numero = 1
